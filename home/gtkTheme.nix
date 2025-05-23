54858b6d38d30472b3c8d8bf0e92405f49540e48
Elias Hanelt <elias.hanelt@gmail.com>
2025-05-23 19:20:29 +0200

    add new catpuccin theme

--- home/gtkTheme.nix
+++ home/gtkTheme.nix
@@ -7,8 +7,11 @@
   gtk = {
     enable = true;
     theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
+      name = "catppuccin-mocha-blue-standard";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["blue"];
+        variant = "mocha";
+      };
     };
     # cursorTheme = {
     #   name = "Bibata-Modern-Ice";
